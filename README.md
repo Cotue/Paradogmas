f64de8f1797d3922c77bb02ac3318e4409bd4e1f
yoel4545 <yoelr086@gmail.com>
2026-09-09 23:28:49 -0600

    KAN-6 main de prueba y readme inicial

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Paradogmas
+
+Para compilar el proyecto ejecute en la terminal:
+->make
+para limpiar la compilacion anterior
+->make clean
+Para ejecutar el programa
+->./cemestre.exe
--- src/main.c
+++ src/main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
-int main(void)
-{
-    printf("Paradogmas Primer Sprint");
-    return 0;
+#include "constants.h"
+#include "models.h"
+
+int main(int argc, char *argv[]) {
+    (void)argc;
+    (void)argv;
+
+    printf("  CEmestre v%s\n", OUTPUT_SCHEMA_VERSION);
+    printf("  Entorno de compilacion verificado.\n");
+
+    return SUCCESS;
 }
